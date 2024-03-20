178a5c198001e42fb8ca66037e809b4bd654db8c
David <castro.3692@gmail.com>
2024-03-20 18:56:49 -0500

    Creacion de base de datos

---
+++ Veterinary.API/Data/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Eventing.Reader;
+using Veterinary.Shared.Entities;
+
+namespace Veterinary.API.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+
+        }
+
+        public DbSet<Owner> Owners { get; set; }
+        //public DbSet<Pet> Pets { get; set; }
+        //public DbSet<PetType> PetTypes { get; set; }
+        //public DbSet<ServiceType> ServiceTypes { get; set; }
+        //public DbSet<History> Histories { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
