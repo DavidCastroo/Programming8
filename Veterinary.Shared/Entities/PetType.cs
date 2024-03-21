6c46ac63c69d7b6062a716c9f95763e19b4bd169
David <castro.3692@gmail.com>
2024-03-20 19:33:27 -0500

    News Tables

---
+++ Veterinary.Shared/Entities/PetType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veterinary.Shared.Entities
+{
+    public class PetType
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Tipo de Mascota")]
+        [MaxLength(50, ErrorMessage = "No se permiten mas de 50 digitos")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Name { get; set; }
+    }
+}
